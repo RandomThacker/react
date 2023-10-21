caf66ae39f8bd60947e9012842d82a7d1a11b2ea
RandomThacker <randomthacker@gmail.com>
2023-10-21 11:59:05 +0530

    toggle done successfully

--- 08themechangerusingcontext/src/App.jsx
+++ 08themechangerusingcontext/src/App.jsx
@@ -1,13 +1,42 @@
+import { ThemeProvider } from './Context/Theme'
+import ThemeBtn from './Components/ThemeBtn'
+import Card from './Components/Card'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector("html").classList.add(themeMode)
+  },[themeMode])
 
   return (
-    <>
-      <h1 className='text-pink-700 text-center py-10'>Hello World</h1>
-    </>
+    <ThemeProvider value = {{themeMode, lightTheme, darkTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                      <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
+
   )
 }
 
 export default App
+  
