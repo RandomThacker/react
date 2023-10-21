cbdcf525fb67269f43d01efb5c065f6456dbb8b9
RandomThacker <randomthacker@gmail.com>
2023-10-21 10:52:43 +0530

    07 done

---
+++ 08themechangerusingcontext/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-pink-700 text-center py-10'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
