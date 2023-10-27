50ffd5323226f51612d21719df1994eb7daee658
RandomThacker <randomthacker@gmail.com>
2023-10-27 11:59:14 +0530

    rodo completed

--- 09todocontextlocal/src/App.jsx
+++ 09todocontextlocal/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoContext, TodoProvider, useTodo } from "./contexts";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -16,9 +18,23 @@ function App() {
     setTodos((prev)=>prev.filter((todo)=> todo.id!==id))
   }
 
-  const toggleComplete = (prev)=>{
-    setTodos((prevTodo)=>prevTodo.map(()=>prevTodo.id===id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo))
+  const toggleComplete = (id)=>{
+    setTodos((prev)=>
+    prev.map((prevTodo)=>
+    prevTodo.id===id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo))
   }
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.lenghth > 0)
+      setTodos(todos)
+ 
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
@@ -27,9 +43,18 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">{/* Todo form goes here */}
+          <TodoForm/>
+            </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+             <div key = {todo.id}
+             className="w-full">
+              <TodoItem todo = {todo} />
+              </div> 
+            ))}
+
           </div>
         </div>
       </div>
